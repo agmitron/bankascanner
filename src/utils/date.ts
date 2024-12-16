e0add0e2c4f828a884d321dac326031ba48b85f4
Aleksei Gmitron <me@agmitron.dev>
2024-12-16 20:43:50 +0400

    #1 T-Bank
    ---------
    
    Co-authored-by: Artiom Brantov <www.arem@mail.ru>

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,4 +1,4 @@
-export const ddmmyyyy = (v: string): Date => {
-    const [day, month, year] = v.split(".");
-    return new Date(`${year}-${month}-${day} 00:00:00`);
+export const ddmmyyyy = (date: string, time: string = "00:00:00"): Date => {
+    const [day, month, year] = date.split(".");
+    return new Date(`${year}-${month}-${day} ${time}`);
 }
