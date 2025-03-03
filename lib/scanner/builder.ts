61a152befb7e3c1334e70878270fb7fe32bfef2d
Aleksei Gmitron <me@agmitron.dev>
2025-03-03 16:35:58 +0400

    +

--- lib/scanner/builder.ts
+++ lib/scanner/builder.ts
@@ -1,6 +1,11 @@
 import { right } from "~/either";
 import type { Attempt, Scan, Scanner } from ".";
-import { DEFAULT_VERSION, UnknownVersionError, type Version, type Versioner } from "./version";
+import {
+	DEFAULT_VERSION,
+	UnknownVersionError,
+	type Version,
+	type Versioner,
+} from "./version";
 import type { Statement } from "~/statement";
 
 /** Represents a part of the statement that should contain an Operation (like row of a table). */
@@ -22,7 +27,7 @@ export function build<V extends Version<string>>(
 	extractors: Record<Version<V>, Extractor>,
 	guessVersion: (s: Statement) => V = () => DEFAULT_VERSION as V,
 ): Versioner<V> {
-	return new class implements Versioner<V> {
+	return new (class implements Versioner<V> {
 		guess(s: Statement): V {
 			return guessVersion(s);
 		}
@@ -32,17 +37,17 @@ export function build<V extends Version<string>>(
 			if (!extractor) {
 				throw new UnknownVersionError(v, name);
 			}
-		
-			return new class implements Scanner {
+
+			return new (class implements Scanner {
 				scan(s: Statement): Scan {
 					const pieces = extractor.pieces(s.content);
 					return pieces.map(extractor.operation);
 				}
-			};
+			})();
 		}
 
 		get supported(): V[] {
 			return Object.keys(extractors) as V[];
 		}
-	}
+	})();
 }
